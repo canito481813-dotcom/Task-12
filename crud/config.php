71046160827fa9e816e6abdb7b78de6bef641f97
canito481813-dotcom <c.anito.481813@umindanao.edu.ph>
2026-03-05 14:51:22 +0800

    Refactor database connection code in config.php

--- crud/config.php
+++ crud/config.php
@@ -1,16 +1,13 @@
 <?php
-
-$host = "localhost";
-$dbname = "car_rental_db";
-$username = "root";
-$password = "";
+$host = 'localhost';
+$dbname = 'car_rental_db';
+$username = 'root';
+$password = '';
 
 try {
-    $pdo = new car_rental_db("mysql:host=$host;dbname=$dbname",
-     $username, $password);
-    $pdo->setAttribute(car_rental_db::ATTR_ERRMODE, car_rental_db::ERRMODE_EXCEPTION);
-
-} catch(car_rental_dbException $e) {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
